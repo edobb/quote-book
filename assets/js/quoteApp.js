ba36dfe26c69d49b6fa91e459fc702b624329ed8
amcnulty <amcnulty88@swbell.net>
2017-10-13 20:46:10 -0700

    Card Creation
    Edited indenting on html.
    Created a mock database in quoteApp.js.
    Moved the Firebase initialization code to quoteApp.js.
    Wrote a method called createAndDisplayCard that takes a quote object and creates html and fills in the data and displays the card to the view.
    The createAndDisplayCard method is being called on startup using the data from the mock database.
    Added a thumbs up image and a thumbs down image.

--- assets/js/quoteApp.js
+++ assets/js/quoteApp.js
@@ -1,3 +1,169 @@
-// window.onload = function(){
-// 	alert("HELLO WORLD");
-// }
+// Quote Book 2017
+// Team Obile
+if (window.attachEvent) {window.attachEvent('onload', load);}
+else if (window.addEventListener) {window.addEventListener('load', load, false);}
+else {document.addEventListener('load', load, false);}
+function load() {
+    /** This data is here just as filler until there is a working database to pull information from. */
+    var mockDatabase = {
+        quotes: [
+            {
+                quote: 'Using the power of decision gives you the capacity to get past any excuse to change any and every part of your life in an instant.',
+                author: 'Tony Robbins',
+                likes: 45,
+                dislikes: 8,
+                dateAdded: 1507402387185,
+                wikiLink: 'http://www.spacex.com/'
+            },
+            {
+                quote: 'Know that although in the eternal scheme of things you are small, you are also unique and irreplaceable, as are all your fellow humans everywhere in the world.',
+                author: 'Margaret Laurence',
+                likes: 29,
+                dislikes: 9,
+                dateAdded: 1507402387285,
+                wikiLink: 'http://www.spacex.com/'
+            },
+            {
+                quote: 'Learning is a treasure that will follow its owner everywhere',
+                author: 'Chinese Proverb',
+                likes: 299,
+                dislikes: 101,
+                dateAdded: 1507402387284,
+                wikiLink: 'http://www.spacex.com/'
+            },
+            {
+                quote: 'A jug fills drop by drop.',
+                author: 'Buddha',
+                likes: 75,
+                dislikes: 15,
+                dateAdded: 1507402387283,
+                wikiLink: 'http://www.spacex.com/'
+            },
+            {
+                quote: 'Yesterday I dared to struggle. Today I dare to win.',
+                author: 'Bernadette Devlin',
+                likes: 8,
+                dislikes: 64,
+                dateAdded: 1507402387282,
+                wikiLink: 'http://www.spacex.com/'
+            },
+            {
+                quote: 'Fear not for the future, weep not for the past.',
+                author: 'Percy Shelley',
+                likes: 632,
+                dislikes: 215,
+                dateAdded: 1507402387281,
+                wikiLink: 'http://www.spacex.com/'
+            },
+            {
+                quote: 'The happiness that is genuinely satisfying is accompanied by the fullest exercise of our faculties and the fullest realization of the world in which we live.',
+                author: 'Bertrand Russell',
+                likes: 187,
+                dislikes: 285,
+                dateAdded: 1507402387280,
+                wikiLink: 'http://www.spacex.com/'
+            },
+            {
+                quote: 'There is no way to happiness, happiness is the way.',
+                author: 'Thich Nhat Hanh',
+                likes: 86,
+                dislikes: 45,
+                dateAdded: 1507402387280,
+                wikiLink: 'http://www.spacex.com/'
+            },
+            {
+                quote: 'Goals are the fuel in the furnace of achievement.',
+                author: 'Brian Tracy',
+                likes: 212,
+                dislikes: 88,
+                dateAdded: 1507402387280,
+                wikiLink: 'http://www.spacex.com/'
+            },
+            {
+                quote: 'When you have got an elephant by the hind legs and he is trying to run away, it\'s best to let him run.',
+                author: 'Abraham Lincoln',
+                likes: 432,
+                dislikes: 636,
+                dateAdded: 1507402387280,
+                wikiLink: 'http://www.spacex.com/'
+            }
+        ]
+    }
+    /** Firebase Initialization Code */
+    var config = {
+        apiKey: "AIzaSyD4l7ohr0vT2IO8HoLQktw68zqTHhkyvck",
+        authDomain: "quote-book-64374.firebaseapp.com",
+        databaseURL: "https://quote-book-64374.firebaseio.com",
+        projectId: "quote-book-64374",
+        storageBucket: "",
+        messagingSenderId: "460460843556"
+    };
+    firebase.initializeApp(config);
+    /** A handle for the firebase database. */
+    var database = firebase.database();
+
+    var quoteApp = {
+        /** The div for displaying quote cards. */
+        quoteDisplay: document.getElementById('quote-display'),
+        /** An array for holding the cards that are in the display. This will can be sorted and used to re display cards in the view. */
+        quoteCards: [],
+        /** Creates card element and adds it to the view.
+         * @param {Object} quote
+         * @param {string} quote.quote
+         * @param {string} quote.author
+         * @param {number} quote.likes
+         * @param {number} quote.dislikes
+         * @param {string} quote.wikiLink
+         * Data about the quote for creating a card to be displayed to the view.
+         * - The actual text content of the quote.
+         * - The author of the quote.
+         * - The number of likes for the quote.
+         * - The number of dislikes for the quote.
+         * - The link to the wikipedia page for this quote.
+         */
+        createAndDisplayCard: function(quote) {
+            var newCard = document.createElement("div");
+            newCard.className = "card";
+            var cardBody = document.createElement("div");
+            cardBody.className = "card-body";
+            var quoteText = document.createElement("h4");
+            quoteText.className = "card-title";
+            quoteText.innerHTML = quote.quote;
+            cardBody.appendChild(quoteText);
+            var quoteAuthor = document.createElement("p");
+            quoteAuthor.className = "card-text";
+            quoteAuthor.innerHTML = quote.author;
+            cardBody.appendChild(quoteAuthor);
+            newCard.appendChild(cardBody);
+            var cardFooter = document.createElement("div");
+            cardFooter.className = "card-header";
+            var wikiLink = document.createElement("a");
+            wikiLink.className = "wikiLink";
+            wikiLink.href = quote.wikiLink;
+            wikiLink.innerHTML = "Wikipedia";
+            cardFooter.appendChild(wikiLink);
+            var thumbsUp = document.createElement("img");
+            thumbsUp.className = "thumbsImg";
+            thumbsUp.src = "assets/images/icons/thumbsUp.png";
+            cardFooter.appendChild(thumbsUp);
+            var likesSpan = document.createElement("span");
+            likesSpan.innerHTML = quote.likes;
+            cardFooter.appendChild(likesSpan);
+            var thumbsDown = document.createElement("img");
+            thumbsDown.className = "thumbsImg";
+            thumbsDown.src = "assets/images/icons/thumbsDown.png";
+            cardFooter.appendChild(thumbsDown);
+            var disLikesSpan = document.createElement("span");
+            disLikesSpan.innerHTML = quote.dislikes;
+            cardFooter.appendChild(disLikesSpan);
+            newCard.appendChild(cardFooter);
+            quoteApp.quoteDisplay.appendChild(newCard);
+        }
+    };
+    /** Temporary self invoking function for displaying the mock database.*/
+    (function() {
+        for (var i = 0; i < mockDatabase.quotes.length; i++) {
+            quoteApp.createAndDisplayCard(mockDatabase.quotes[i]);
+        }
+    })();
+}
